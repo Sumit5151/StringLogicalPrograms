dd0c2bd2ee8866596b1957294889e80377823534
Sumit5151 <sumitshitole5151@gmail.com>
2022-12-03 09:55:54 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using StringLogicalPrograms;
+
+
+
+Console.WriteLine();
+StringDemoProgarms stringDemoProgarms = new StringDemoProgarms();
+//stringDemoProgarms.ReverseString("Welcome");
+//stringDemoProgarms.ReverseEachWord();
+stringDemoProgarms.PrintTheCharacterWhichOccursOneTime();
+
+
+
+Console.ReadLine();
---
+++ StringDemoProgarms.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringLogicalPrograms
+{
+    internal class StringDemoProgarms
+    {
+
+
+        public void ReverseEachWord()
+        {
+            string str = "Welcome in Pune City";
+
+            var stringArray = str.Split(' ');
+
+            for (int i = 0; i < stringArray.Length; i++)
+            {
+                ReverseString(stringArray[i]);
+                Console.Write(" ");
+            }
+        }
+
+        public void ReverseString(string str)
+        {            
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                Console.Write(str[i]);
+            }
+        }
+
+
+
+        public void PrintTheCharacterWhichOccursOneTime()
+        {
+            string str = "Hello World";           
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                int count = 0;
+
+                for (int j = 0; j < str.Length; j++)
+                {
+
+                    if(str[i] == str[j])
+                    {
+                        count++;
+                    }
+                }
+
+                if(count == 1)
+                {
+                    Console.Write(str[i]);
+                }
+
+            }
+        }
+
+
+
+
+    }
+}
